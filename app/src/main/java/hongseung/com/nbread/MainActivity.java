6a5cd5832bd1676768a8cb68689ec18f0f329645
Seunghyun.hong <hong.seung.hyun.23@gmail.com>
2017-08-31 16:26:46 +0900

    새로 만들려고

--- app/src/main/java/hongseung/com/nbread/MainActivity.java
+++ app/src/main/java/hongseung/com/nbread/MainActivity.java
@@ -1,45 +1,29 @@
 package hongseung.com.nbread;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements ScreenFragment.OnSendMessageListener {
 
-    private ScreenFragment mFragment1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 액티비티에 프래그먼트 연결
+        ScreenFragment fragment = new ScreenFragment();
 
-        mFragment1 = new ScreenFragment();
-
+        // R.id.container 영역에 프래그먼트를 붙이겠다.
+        // 근데 굳이 동적으로 붙일 필요가... 없긴 한데..
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.container1, mFragment1)
+                .add(R.id.container1, fragment)
                 .commit();
+
     }
 
-    // 메시지를 뿌린다.
     @Override
     public void onSendMessage(String message) {
-        mFragment1.sendMessage(message);
-    }
 
-    // 이건 모든앱?! 에서 된다!
-    public void shareButtonClicked(View view) {
-        String message = mFragment1.shareMessage();
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, message);
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
     }
 }
 
